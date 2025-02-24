191dde6b334c359f54304d4244189c54aeb5585a
Husayn / Fionn <124906203+HusaynPrograms@users.noreply.github.com>
2025-02-24 12:08:54 +0000

    Update app.component.ts
    adding comments

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,12 +6,13 @@ import { BComponent } from './b/b.component';
 
 @Component({
   selector: 'root-directive',
-  imports: [RouterOutlet, AComponent, BComponent],
+  imports: [RouterOutlet, AComponent, BComponent], // Imports components
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'my-first-app';
+  // Declaring Variables
   name: string = "fionn"
   age: number = 21
 }
